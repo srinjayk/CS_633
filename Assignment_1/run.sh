c7dd999ceadd6b5195bb65063c8791f05fbc4622
Srinjay Kumar <35092522+srinjayk@users.noreply.github.com>
2021-02-21 21:19:37 +0530

    Update run.sh

--- Assignment_1/run.sh
+++ Assignment_1/run.sh
@@ -1,4 +1,6 @@
-for P in 16 36 49 64
+touch out.csv
+touch out
+for P in 16 25 36 49 64
 do
 	touch $P
 	for N in 256 1024 4096 16384 65536 262144 1048576
@@ -7,7 +9,13 @@ do
 		# echo $P
 		for m in 1 2 3 4 5
 		do
-			mpirun -np $P -hosts csews20,csews23,csews24,csews25,csews26,csews27,csews28,csews29,csews30 ./src $N 50 >> $P 
+			BOUT=$(mpirun -np $P -hosts csews20,csews23,csews24,csews25,csews26,csews27,csews28,csews29,csews30 ./src $N 50)
+			echo "${BOUT}"
+			for A in ${BOUT}
+			do
+				echo $P,$N,$A >> out.csv
+				echo $A >> out
+			done
 		done
 	done
 done
